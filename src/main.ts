9f09c511ea1213b850e6a9d5837f65243ea27f12
ToKyun02 <developtk02@gmail.com>
2026-03-27 17:19:25 +0900

    :sparkles: add to auth route

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookiesSession = require('cookie-session');
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    (app as any).set('etag', false);
-    app.use((req, res, next) => {
-        res.removeHeader('x-powered-by');
-        res.removeHeader('date');
-        next();
-    });
+    app.use(
+        cookiesSession({
+            keys: ['asdf'],
+        }),
+    );
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
