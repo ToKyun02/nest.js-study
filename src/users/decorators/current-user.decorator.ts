33a4cf7b24e33de0159b7cea587da38ce604ff05
Tokyun02 <developtk02@gmail.com>
2026-04-06 19:52:33 +0900

    auth guard

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -2,6 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
   (data: any, context: ExecutionContext) => {
-    return 'hi there!';
+    const req = context.switchToHttp().getRequest();
+    return req.currentUser;
   },
 );
